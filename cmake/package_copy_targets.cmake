23e6bbea0e4bdc96ebd2d6149e71c5d8ca049b5a
Angelo Theodorou <encelo@gmail.com>
2019-03-14 20:50:24 +0100

    Add support for new nCine discoverability system
    - Use nCine interface compile options instead of redefining them
    - Link against the ncine_main static library
    - Fix the CMake version retrieval script when there is no git repository

--- cmake/package_copy_targets.cmake
+++ cmake/package_copy_targets.cmake
@@ -16,13 +16,11 @@ elseif(WIN32)
 		set_target_properties(copy_shaders PROPERTIES FOLDER "CustomCopyTargets")
 	endif()
 
-	if(DEFINED NCINE_BUILD_DIR OR DEFINED NCINE_INSTALL_DIR)
-		add_custom_target(copy_ncine_dll ALL
-			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${NCINE_DLL} ${CMAKE_BINARY_DIR}
-			COMMENT "Copying nCine DLL..."
-		)
-		set_target_properties(copy_ncine_dll PROPERTIES FOLDER "CustomCopyTargets")
-	endif()
+	add_custom_target(copy_ncine_dll ALL
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different ${NCINE_LOCATION} ${CMAKE_BINARY_DIR}
+		COMMENT "Copying nCine DLL..."
+	)
+	set_target_properties(copy_ncine_dll PROPERTIES FOLDER "CustomCopyTargets")
 endif()
 
 if(MSVC)
